d86918eebc552b33f0a5bd1be641e7b0ded38233
Brandon Brooks <brandonbrbrooks@gmail.com>
2024-06-09 18:53:47 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+
+const menu = document.querySelector(mobile-menu);
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
 function alertButton() {
     alert("hello world")
 }
-function workButton() {
-    alert("work")
+function games() {
+    alert("games")
 }
